0d35edbf30acf4827c58f13fc359c114600e0bd3
desomb <vince.29200@hotmail.fr>
2019-02-22 14:39:34 +0100

    Added basic footer/header
    Added basic footer/header

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TradeFriendlyFooter extends Component {
+    render() {
+        return(
+            <footer>
+                <p>
+                    test
+                </p>
+            </footer>
+        )
+    }
+}
+
+export default TradeFriendlyFooter;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+export function TradeFriendlyHeader() {
+        return (
+            <header className="App-header"> 
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        )
+}
