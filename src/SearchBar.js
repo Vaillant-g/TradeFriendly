0f8a5f9c6efedda4170827d15de0c9e1e4eff449
Gvaillant <gautier.vaillant@epitech.eu>
2019-02-22 20:53:27 +0100

    ajout logo

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
+import logo from './Tradly.png'
 export function TradeFriendlySearchBar() {
         return (
             <div className="App-SearchBar">
             
+            <img src={logo} width="500" height="500" />
                 Content
             </div>
         )
