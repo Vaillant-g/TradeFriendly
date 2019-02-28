695aed1847a9aa5b0d8ee37b5b393d8314ca5b3f
desomb <vince.29200@hotmail.fr>
2019-02-28 12:18:53 +0100

    Added steam button co
    Added steam button co

--- TradeFriendlyClient/src/Header.js
+++ TradeFriendlyClient/src/Header.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import logo from './Logo.png'
+import logo from './Logo.png';
+import SteamCo from './Steam.png';
 
 function ShowAlert() {
     alert("test");
 }
 
+const ImgStyle = {
+    width: 127
+};
+
 export function TradeFriendlyHeader() {
         return (
             <header className="App-header"> 
                 <img src={logo} className="App-logo" />
-                <button className="SteamCoButton" onClick={ShowAlert}>Connexion</button>
+                <button className="SteamCoButton" onClick={ShowAlert}><img style={ImgStyle} src={SteamCo} ></img></button>
             </header>
         )
 }
