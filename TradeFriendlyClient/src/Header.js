3a536550dd3dc219f67867ff30f24053b52f712a
desomb <vince.29200@hotmail.fr>
2019-02-28 10:52:22 +0100

    Added co button
    Added co button

--- TradeFriendlyClient/src/Header.js
+++ TradeFriendlyClient/src/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import logo from './Logo.png'
 
+function ShowAlert() {
+    alert("test");
+}
+
 export function TradeFriendlyHeader() {
         return (
             <header className="App-header"> 
-            <img src={logo} className="App-logo" />
+                <img src={logo} className="App-logo" />
+                <button className="SteamCoButton" onClick={ShowAlert}>Connexion</button>
             </header>
         )
 }
