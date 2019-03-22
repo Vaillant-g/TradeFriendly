b9938af16c7bd8bbfd4b0a717c5ee8c514cc789d
desomb <vince.29200@hotmail.fr>
2019-03-22 11:03:30 +0100

    Server api steam updated + dependencies added
    Server api steam updated + dependencies added

--- TradeFriendlyServ/TfApp.js
+++ TradeFriendlyServ/TfApp.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const request = require('request');
+
 
 
 const port = process.env.PORT || 4001;
+const steamUrl = "";
 const index = require("./routes/index");
 const app = express();
 app.use(index);
@@ -14,7 +17,7 @@ io.on("connection", function(socket) {
     console.log("New client connected");
 
     socket.on('steamCo', function(data) {
-        console.log("Victime");
+        victime();
     });
 
     socket.on("disconnect", () => console.log("Client disconnected"));
@@ -23,3 +26,12 @@ io.on("connection", function(socket) {
 
   server.listen(port, () => console.log(`Listening on port ${port}`));
 
+  function victime()
+  {
+    console.log("Victime");
+        var url = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/' +
+            'v2/?key=3F95133360C48472452698ACF9529A0F&appid=8930';
+        request.get(url, function(error, steamHttpResponse, steamHttpBody) {
+            console.log('test:', steamHttpResponse);
+        });
+  }
