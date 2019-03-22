15af2c7940d83e9e6d15f31f0f8bd825626d3474
desomb <vince.29200@hotmail.fr>
2019-03-22 18:11:36 +0100

    Added dependencies for steam api on serv
    Added dependencies for steam api on serv

--- TradeFriendlyServ/TfApp.js
+++ TradeFriendlyServ/TfApp.js
@@ -2,16 +2,41 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const request = require('request');
+const passport = require('passport');
+const steamUserInventory = require('steam-user-inventory');
+const steamfriends = require('web-api-steam');
 
 
 
 const port = process.env.PORT || 4001;
-const steamUrl = "";
-const index = require("./routes/index");
+//const index = require("./routes/index");
 const app = express();
-app.use(index);
 const server = http.createServer(app);
+
 const io = socketIo(server); // < Interesting!
+let divet;
+
+
+  server.listen(port, () => console.log(`Listening on port ${port}`));
+
+  function victime()
+  {
+
+    steamfriends.getFriendList("76561198072811191", "3F95133360C48472452698ACF9529A0F", (err, data) => {
+        console.log(data);
+        
+    });
+
+    /*steamUserInventory('hiepbinh', "730/2/").then(data => {
+        data.forEach(function(entry) {
+         console.log(entry.name);
+        });
+    });*/
+
+
+    console.log("Victime");
+  }
+
 
 io.on("connection", function(socket) {
     console.log("New client connected");
@@ -23,15 +48,4 @@ io.on("connection", function(socket) {
     socket.on("disconnect", () => console.log("Client disconnected"));
   });
 
-
-  server.listen(port, () => console.log(`Listening on port ${port}`));
-
-  function victime()
-  {
-    console.log("Victime");
-        var url = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/' +
-            'v2/?key=3F95133360C48472452698ACF9529A0F&appid=8930';
-        request.get(url, function(error, steamHttpResponse, steamHttpBody) {
-            console.log('test:', steamHttpResponse);
-        });
-  }
+  
